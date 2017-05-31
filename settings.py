a99562a9519d0ea4c66823277f30e3736767e948
Marcin Kołda <marcin.kolda@gmail.com>
2017-05-31 23:45:23 +0200

    reverted settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
-CLIENT_ID = '959295031501-5u6kqd1luqvfo11h78fuc2o8m4l9i05c.apps.googleusercontent.com'
-CLIENT_SECRET = 'DSCKfyLL0Tgbub9f55mSJzc-'
+CLIENT_ID = 'client_id'
+CLIENT_SECRET = 'client_secret'
 SCOPE = ['https://www.googleapis.com/auth/calendar', 'https://www.googleapis.com/auth/userinfo.email']
-SECRET_KEY = '+DAzF0giyYuvJBZ8llIKq6hp3wpMeeQ1Ktz2IQP3dfk='
-GIT_COMMIT = 'eda144c'
-BUILD_DATE = '2017-05-31 21:12:42'
+SECRET_KEY = 'secret_key_update_before_deployment'
+GIT_COMMIT = 'git_commit'
+BUILD_DATE = 'build_date'
