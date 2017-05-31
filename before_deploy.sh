fb48983fe1db363dd1896785a28034270b532913
Marcin Kołda <marcin.kolda@gmail.com>
2017-05-31 23:02:40 +0200

    Travis CI, OAuth client secrets are now taken from secured env variables

--- before_deploy.sh
+++ before_deploy.sh
@@ -7,8 +7,3 @@ sed -i -e "s/git_commit/${git_commit}/g" settings.py
 sed -i -e "s/build_date/${build_date}/g" settings.py
 sed -i -e "s/client_id/${OOO_MANAGER_CLIENT_ID}/g" settings.py
 sed -i -e "s/client_secret/${OOO_MANAGER_CLIENT_SECRET}/g" settings.py
-
-gcloud app deploy app.yaml --version ooo-manager-1-0 --project out-of-office-manager -q
-
-git checkout -- settings.py
-rm settings.py-e
